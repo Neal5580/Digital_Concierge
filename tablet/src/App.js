e3101854aa312250d1b649fc0dd8f95329b31d54
Neal Shen <>
2018-11-22 16:32:14 +1100

    added tablet project back

--- tablet/src/App.js
+++ tablet/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Tablet from "./Tablet";
 class App extends Component {
     componentDidMount() {
-        fetch("http://localhost:3000/")
+        /*fetch("http://localhost:3000/")
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => console.log(data));*/
     }
 
     render() {
-        console.log("123");
-        return (
-            <Router basename={"tablet"}>
-                <Route path="/" component={Tablet} />
-            </Router>
-        );
+        return <h1>TABLET PROJECT </h1>;
     }
 }
 
