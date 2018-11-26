3ecc02e564bbebd9728f83064d60bc42d141d972
Neal Shen <>
2018-11-26 15:22:38 +1100

    update npm package file

--- touchscreen/src/App.js
+++ touchscreen/src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    componentDidMount() {
+        fetch("/api/")
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+    render() {
+        return (
+            <Router
+                ref={router => (this.router = router)}
+                basename={"touchscreen"}
+            >
+                <div>
+                    123
+                    <section>
+                        <div>
+                            <Route
+                                path="/"
+                                exact
+                                render={() => (
+                                    <h1>TOUCHSCREEN HOME PAGE 123</h1>
+                                )}
+                            />
+                            <Route
+                                path="/test"
+                                render={() => (
+                                    <h1>TOUCHSCREEN HOME PAGE TEST</h1>
+                                )}
+                            />
+                        </div>
+                    </section>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
