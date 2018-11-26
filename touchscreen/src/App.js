cdfd80632202b36e7774dcc94020804119ce4599
Neal Shen <>
2018-11-26 11:44:32 +1100

    recreate touchscreen project

--- touchscreen/src/App.js
+++ touchscreen/src/App.js
@@ -1,44 +1,28 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-    componentDidMount() {
-        fetch("/api/")
-            .then(response => response.json())
-            .then(data => console.log(data));
-    }
-
-    render() {
-        return (
-            <Router
-                ref={router => (this.router = router)}
-                basename={"touchscreen"}
-            >
-                <div>
-                    123
-                    <section>
-                        <div>
-                            <Route
-                                path="/"
-                                exact
-                                render={() => (
-                                    <h1>touchscreen HOME PAGE 123</h1>
-                                )}
-                            />
-                            <Route
-                                path="/test"
-                                render={() => (
-                                    <h1>touchscreen HOME PAGE TEST</h1>
-                                )}
-                            />
-                        </div>
-                    </section>
-                </div>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
